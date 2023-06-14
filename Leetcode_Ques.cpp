0eb6e14b45a6555cd970b840f41f8bc0e3e37535
Yash Purwar <nayan251198@gmail.com>
2023-06-14 22:48:51 +0530

    Leetcode Question

--- Leetcode_Ques.cpp
+++ Leetcode_Ques.cpp
@@ -38,3 +38,62 @@
 //     }
 //     cout<<count;
 // }
+
+
+// Code for converting decimal into binary
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+// int main(){
+//     int N;
+//     cin>>N;
+//     int a,b,rev=0;
+//     int i=0;
+//     while(N!=0){
+//         a = N%2;
+//         rev = (a * pow(10, i)) + rev;
+//         i=i+1;
+//         N=N/2;
+//     }
+//     cout<<rev;
+// }
+
+
+// Code for converting binary into decimal
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+// int main(){
+//     int N;
+//     cin>>N;
+//     int i=0, add=0;
+//     while(N!=0){
+//         int digit = N%10;
+//         if(digit==1){
+//             add = pow(2,i) + add;
+//         }
+//          N=N/10;
+//          i++;
+//     }
+//     cout<<add;
+// }
+
+
+// Reverse the Integer
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+// int main(){
+//     int N, a, b;
+//     cin>>N;
+//     int rev=0;
+//     while(N!=0){
+//         a=N%10;
+//         if((rev>INT16_MAX/10)||(rev<INT16_MIN/10)){
+//              return 0;
+//         }
+//         rev = (rev*10)+a;
+//      N=N/10;
+//     }
+//     cout<<rev;
+// }
