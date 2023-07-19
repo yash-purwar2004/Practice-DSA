a16f328425534a55ba3d8febbbfc1ac180eb3e94
yash-sans2004 <nayan251198@gmail.com>
2023-07-20 00:12:55 +0530

    Sum of Two Arrays ~ Leetcode Question

--- Leetcode_Ques.cpp
+++ Leetcode_Ques.cpp
@@ -1,28 +1,28 @@
 // Subtract the product and the sum of digits of an integer
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int N;
-//     int a,b, sum=0, pro=1, diff;
-//     cin>>N;
-//     while(N!=0){
-//         a = N%10;
-//         b = N/10;
-//         cout<<a<<endl;
-//         N=b;
-//         sum = sum + a;
-//         pro = pro * a;
-//     }
-//     cout<<"The sum of the digit is "<<sum<<endl;
-//     cout<<"The product of the digit is "<<pro<<endl;
-//     if(sum>pro){
-//         cout<<"The difference of sum and product is "<<sum-pro<<endl;
-//     }
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+    int a,b, sum=0, pro=1, diff;
+    cin>>N;
+    while(N!=0){
+        a = N%10;
+        b = N/10;
+        cout<<a<<endl;
+        N=b;
+        sum = sum + a;
+        pro = pro * a;
+    }
+    cout<<"The sum of the digit is "<<sum<<endl;
+    cout<<"The product of the digit is "<<pro<<endl;
+    if(sum>pro){
+        cout<<"The difference of sum and product is "<<sum-pro<<endl;
+    }
 
-//     else if(pro>sum){
-//         cout<<"The difference of the sum and product is "<<pro-sum<<endl;
-//     }
-// }
+    else if(pro>sum){
+        cout<<"The difference of the sum and product is "<<pro-sum<<endl;
+    }
+}
 
 // Find the number of 1 bits
 // #include<iostream>
@@ -333,3 +333,61 @@
 //       }
 //    }
 // }
+
+
+// Sum of Two Arrays
+#include <bits/stdc++.h> 
+vector<int>reverse(vector<int>v){
+    int s = 0;
+    int e = v.size()-1;
+
+    while(s<e){
+        swap(v[s++], v[e--]);
+    }
+
+    return v;
+}
+
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	int i = n-1;
+    int j = m-1;
+    int carry = 0;
+    vector<int>ans;
+
+    while(i>=0 && j>=0){
+        int val1 = a[i];
+        int val2 = b[j];
+        int sum = val1 + val2 + carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+        i--;
+        j--;
+    }
+
+    while(i>=0){
+        int sum = a[i] + carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+        i--;
+    }
+
+    while(j>=0){
+        int sum = b[j] + carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+        j--;
+    }
+
+
+    while(carry!=0){
+        int sum = carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+    }
+
+    return reverse(ans);
+}
