eec93a35dedac5eedc3fb58bf9339f19eedd7de9
yash-sans2004 <nayan251198@gmail.com>
2023-07-21 21:06:04 +0530

    Leetcode questions

--- Leetcode_Ques.cpp
+++ Leetcode_Ques.cpp
@@ -433,3 +433,43 @@ int main(){
 
 //     return true;
 // }
+
+
+
+// code for maximum occuring character in a string
+#include <iostream>
+#include <string>
+using namespace std;
+
+char getmaxcharacter(string s) {
+    int arr[26] = {0}; // Initialize the array with zeros
+
+    for (int i = 0; i < s.length(); i++) {
+        char ch = s[i];
+        int number = 0;
+
+        if (ch >= 'a' && ch <= 'z') {
+            number = ch - 'a';
+        } else if (ch >= 'A' && ch <= 'Z') { // Use 'else if' here
+            number = ch - 'A';
+        }
+        arr[number]++;
+    }
+
+    int maxi = -1, ans = 0;
+    for (int i = 0; i < 26; i++) { // Correct the loop limit to 26
+        if (maxi < arr[i]) { // Use '<' instead of '='
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    char finalans = 'a' + ans;
+    return finalans;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << getmaxcharacter(s) << endl;
+    return 0;
+}
