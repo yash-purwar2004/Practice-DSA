48b4cf3f01fcf132331bb69de134ebaa8dda7ff4
yash-sans2004 <nayan251198@gmail.com>
2023-08-03 19:12:07 +0530

    Pointers & Function

--- Pointers.cpp
+++ Pointers.cpp
@@ -28,3 +28,70 @@ int main() {
     cout<<t;
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10]={8,7,3,4,5,6,7,8,9,0};
+    int *ptr = &arr[0];
+    cout<<sizeof(arr)<<endl;
+    cout<<sizeof(ptr)<<endl;
+    cout<<sizeof(*ptr)<<endl;
+    cout<<arr<<endl;
+    cout<<&arr[1]<<endl;
+    cout<<*arr<<endl;
+    cout<<*(arr+1)<<endl;
+    cout<<*arr+1<<endl;
+    int i=4;
+    cout<<i[arr]<<endl;
+    return 0;
+}
+
+// Create a character array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5]={1,2,3,4,5};
+    char ch[6]={"abcde"};
+    char *p = "abcde";
+    cout<<*(p+1)<<endl;
+
+    cout<<arr<<endl;
+    cout<<ch<<endl;
+    
+    char *c = &ch[0];
+    // print entire string
+    cout<<*c<<endl;
+    
+    // Jab tk null character nhi milega tab tk print hota jayega
+    char temp='x';
+    char *ptr = &temp;
+    cout<<ptr;
+    
+    return 0;
+}
+
+
+
+
+// Pointers & Functions
+#include <iostream>
+using namespace std;
+int update(int *nm){
+    *nm=*nm+1;
+    return *nm;
+}
+
+int upar(int *arr){
+    return *(arr+1);
+}
+
+int main() {
+    int num = 123;
+    cout<<update(&num)<<endl;
+    
+    int arr[5]={1,2,3,4,5};
+    cout<<upar(arr)<<endl;
+    return 0;
+}
