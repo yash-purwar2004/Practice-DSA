0584bb59e01fc20ada6182c81ba89573bb2261be
yash-sans2004 <nayan251198@gmail.com>
2023-08-23 23:59:45 +0530

    Code of 2 raise to power n by using recursion

--- Recursion.cpp
+++ Recursion.cpp
@@ -19,3 +19,25 @@ int main()
     int ans = factorial(n);
     cout<<ans<<endl;
 }
+
+
+
+// Code of 2 raise to power 'n' by using factorial
+#include<iostream>
+using namespace std;
+int power(int n){
+    if(n == 0){
+        return 1;
+    }
+    
+    int smaller = power(n-1);
+    int bigger = 2 * smaller;
+    return bigger;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int ans = power(n);
+    cout<<ans<<endl;
+}
