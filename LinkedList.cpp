eed8882e2cecaa24c0b8add6424bc0ab4c8bfb00
yash-sans2004 <nayan251198@gmail.com>
2023-09-04 01:27:21 +0530

    At the any position insertion in Linked List and iterate over the linked list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -95,3 +95,65 @@ int main(){
     insertAtTail(tail, 21);
     print(head);
 }
+
+
+
+// At the any position insertion in Linked List and iterate over the linked list
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+
+void insertAtPosition(Node* &tail, Node* &head, int position, int d){
+    if(position == 1){
+        insertAtHead(head, d);
+        return;
+    } 
+     
+    Node *temp = head;
+    int cnt = 1;
+    while(cnt< position-1){
+        temp = temp -> next;
+        cnt++;
+    }
+    
+    
+    if(temp->next == NULL){
+        insertAtTail(tail,d);
+        return;
+    }
+    
+    Node* node = new Node;
+    node -> data = d;
+    node->next = temp -> next;
+    temp->next = node;
+    
+}
+
+
+ void print(Node* &head){
+    Node* temo = head; // alag se pointer bana lia
+    while(temo!=NULL){
+        cout<<temo -> data<<" ";
+        temo = temo -> next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* node1 = new Node;
+    Node* node2 = new Node;
+    
+    Node* head = node1;
+    
+    node1 -> data = 12;
+    node1 ->next = node2;
+    node2 ->data = 13;
+    node2 ->next = NULL;
+    
+    insertAtPosition(head, 2, 22);
+    print(head);
+}
