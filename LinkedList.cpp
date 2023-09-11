ac5ed276b6662105c90905bc56bd78ce6f6db8aa
yash-sans2004 <nayan251198@gmail.com>
2023-09-12 00:21:57 +0530

    Code for deleting the first or last or any node in linked list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -157,3 +157,90 @@ int main(){
     insertAtPosition(head, 2, 22);
     print(head);
 }
+
+
+
+
+// Code for deleting the first or last or any node in linked list
+#include<iostream>
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* next;
+};
+
+void insertAtTail(Node* &tail, int d){
+    Node* temp = new Node;
+    temp -> data = d;
+    temp->next= NULL;
+    tail -> next = temp;
+    tail = temp;
+}
+
+void print(Node* &head){
+        Node* temo = head; // alag se pointer bana lia
+        while(temo!=NULL){
+            cout<<temo -> data<<" ";
+            temo = temo -> next;
+        }
+        cout<<endl;
+    }
+    
+void deleteNode(int position, Node* &head, Node* &tail){
+    // deleting first or starting Node
+    if(position==1){
+        Node*temp = head;
+        head = head -> next;
+        temp -> next = NULL;
+        delete temp;
+    }
+    
+    else{
+        // deleting any middle node or last node
+        Node* curr = head;
+        Node* prev = NULL;
+        
+        int cnt=1;
+        while(cnt < position){
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+        
+        prev -> next = curr -> next;
+        curr -> next = NULL;
+        delete(curr);
+        
+         if (curr == tail) {
+            tail = prev;
+        }
+    }
+}
+
+int main(){
+    Node* node1 = new Node;
+    node1 -> data = 30;
+    node1 -> next = NULL;
+    
+    Node* head = node1;
+    Node* tail = node1;
+    
+    insertAtTail(tail, 32);
+    insertAtTail(tail, 2);
+    insertAtTail(tail, 3);
+    insertAtTail(tail, 21);
+    print(head);
+    
+    
+    cout<<"head deleted"<<endl;
+    deleteNode(1, head, tail);
+    print(head);
+    deleteNode(4, head, tail);
+    print(head);
+    
+    cout<<head->data<<endl;
+    cout<<tail->data<<endl;
+    
+}
