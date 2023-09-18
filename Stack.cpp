4a26cd24acd9ebd698171847bc1f914bb1207d5f
yash-sans2004 <nayan251198@gmail.com>
2023-09-19 00:28:59 +0530

    Implementation of Stack using Class

--- Stack.cpp
+++ Stack.cpp
@@ -29,3 +29,76 @@ int main(){
 }
 
 
+
+// Implementation of Stack using Class
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Stack{
+    public:
+    int top = -1;
+    int size;
+    int *arr = new int[size];
+    
+    
+    void push(int element){
+        if((size-top)>1){
+            top++;
+            arr[top] = element;
+        }
+        
+        else{
+            cout<<"Stack Overflow "<<endl;
+        }
+    }
+    
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        
+        else{
+            cout<<"Stack Underflow "<<endl;
+        }
+    }
+    
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }
+        
+        else{
+            cout<<"Stack is Empty "<<endl;
+            return -1;
+        }
+    }
+    
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+    }
+};
+
+int main(){
+    Stack s;
+    s.size = 5;
+    s.push(22);
+    s.push(43);
+    s.push(44);
+    s.push(54);
+    
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    s.pop();
+    s.pop();
+
+    
+}
