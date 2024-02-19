f8c7032c3955e6dd750cdd66c9071e22858ce069
yash-sans2004 <nayan251198@gmail.com>
2024-02-19 23:41:55 +0530

    Iterative Inorder Traversal using Stack

--- Trees.cpp
+++ Trees.cpp
@@ -54,3 +54,131 @@ int main(){
     printLevelOrder(root);
     return 0;
 }
+
+
+
+
+
+
+
+// Pre Order Traversal using Stack
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int data){
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void PreOrderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    
+    stack<Node*>s;
+    s.push(root);
+    
+    while(s.empty() == false){
+        Node* temp = s.top();
+        cout<<temp->data<<" ";
+        s.pop();
+        
+        if (temp->right != NULL){
+            s.push(temp->right);
+        }
+ 
+        // Enqueue right child
+        if (temp->left != NULL){
+            s.push(temp->left);
+        }
+    }
+}
+
+int main(){
+    Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(7);
+    root->left->left = newNode(3);
+    root->left->right = newNode(4);
+    root->left->right->left = newNode(5);
+    root->left->right->right = newNode(6);
+    
+    cout << "Pre Order traversal of binary tree is \n";
+    PreOrderTraversal(root);
+}
+
+
+
+
+
+
+
+
+
+// In Order Traversal Using Stack
+#include<iostream>
+#include<stack>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+};
+
+
+void inOrderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    
+    stack<Node*>s;
+    Node* temp = root;
+    
+    while(temp != NULL || s.empty() == false){
+        while (temp != NULL) {
+            s.push(temp);
+            temp = temp->left;
+        }
+        
+        temp = s.top();
+        s.pop();
+        cout<<temp->data<<" ";
+        
+        temp = temp->right;
+    }
+    
+}
+
+
+Node* newNode(int data){
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+
+int main(){
+    Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->left->right->left = newNode(6);
+    root->left->right->right = newNode(7);
+    
+    cout << "In Order traversal of binary tree is \n";
+    inOrderTraversal(root);
+    return 0;
+}
